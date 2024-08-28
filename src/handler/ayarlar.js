46150f0b8521e16a475133e9389052544d1b46a3
ArseN <77533226+CemilGrl@users.noreply.github.com>
2024-08-29 01:49:18 +0300

    Update ayarlar.js

--- src/handler/ayarlar.js
+++ src/handler/ayarlar.js
@@ -1,5 +1,3 @@
 module.exports = {
-    hex: "1c0f45", //embedlerin varsayılan hex kodu
-    tick: "✅", //komutlardaki tick emojisi
-    cross: "❎" //komutlardaki çarpı emojisi
+    hex: "1c0f45" //embedlerin varsayılan hex kodu
 }
